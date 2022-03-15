81b6af4ec87e4464ea5cebe29cdb19736e8c9692
cristalNichita <kristal.nikita@outlook.com>
2022-03-15 16:24:32 +0200

    pagination, search, creating, sort, routing

--- src/App.js
+++ src/App.js
@@ -1,55 +1,23 @@
-import axios from "axios";
-import React, { useState, useMemo } from "react";
-import PostFilter from "./components/PostFilter";
-import PostForm from "./components/PostForm";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyModal from "./components/UI/MyModal/MyModal";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './css/App.css';
-import { usePosts } from "./hooks/usePosts";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
 
 function App() {
 
-	const [posts, setPosts] = useState([]);
-
-	const [filter, setFilter] = useState({sort: '', query: ''});
-	const [modal, setModal] = useState(false);
-	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-	const createPost = (newPost) => {
-		setPosts([...posts, newPost]);
-		setModal(false);
-	}
-
-	async function fetchPosts() {
-		const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-		setPosts(response.data);
-	}
-
-	const removePost = (post) => {
-		setPosts(posts.filter(p => p.id !== post.id));
-	}
-
 	return (
-		<div className="App">
-			<button onClick={fetchPosts}>клик</button>
-			<MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-				Создать пост
-			</MyButton>
-			<MyModal visible={modal} setVisible={setModal}>
-				<PostForm create={createPost} />
-			</MyModal>
-
-			<hr style={{margin: '15px 0'}}></hr>
-
-			<PostFilter 
-				filter={filter} 
-				setFilter={setFilter} 
-			/>
-
-			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
-		</div>
-	);
+		<BrowserRouter>
+			<div className="navbar">
+				
+			</div>
+			<Routes>
+				<Route path="/about" element={<About/>} />
+				<Route path="/posts" element={<Posts/>} />
+			</Routes>
+		</BrowserRouter>
+	)
+	
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <h1>
+            Это что-то я не знаю что.
+        </h1>
+    )
+}
+
+export default About
